65565e7fc27385d3b053a4c39734af6e3bb0041f
Tami Hernandez <tamianna.h@gmail.com>
2025-05-22 15:44:43 -0500

    feat: add main tags& section tag with main text to Main.jsx.

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -2,9 +2,13 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 
 function Main() {
     return (
-        <>
+        <main>
         <WeatherCard />
-        </>
+        <section className="main__container">
+            <p className="main__text">Today is 75 &deg; F / You may want to wear:</p>
+            {/* Add card section. */}
+        </section>
+        </main>
     )
 }
 
