56c3de1bb69b1b29238aaa81c38287b5ac355dd3
Tami Hernandez <tamianna.h@gmail.com>
2025-05-22 15:51:18 -0500

    style: change padding to header. refactor: class names in Main.jsx.

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -4,8 +4,8 @@ function Main() {
     return (
         <main>
         <WeatherCard />
-        <section className="main__container">
-            <p className="main__text">Today is 75 &deg; F / You may want to wear:</p>
+        <section className="cards">
+            <p className="cards__text">Today is 75 &deg; F / You may want to wear:</p>
             {/* Add card section. */}
         </section>
         </main>
