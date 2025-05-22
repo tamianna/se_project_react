544b284986ef79db5c556503101ba58b0fe9fad9
Tami Hernandez <tamianna.h@gmail.com>
2025-05-22 14:00:35 -0500

    feat: add to function Header, import Header.css to Header.jsx and add header logo/avatar to images folder.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,16 @@
+import "./Header.css";
+
 function Header() {
     return (
-        <header>HELLOO!</header>
+        <header className="header">
+            <img className="header__logo"/>
+            <p className="header__date-and-location">DATE. LOCATION.</p>
+            <button className="header__add-clothes-btn">+ ADD CLOTHES</button>
+            <div className="header__user-container">
+                <p className="header__username">NAME 
+                    <img src="" alt="" className="header__avatar" /></p>
+            </div>
+        </header>
     )
 }
 
