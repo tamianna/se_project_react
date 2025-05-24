eeaabfd1890dc0691562661e3577e1db4edebdb4
Tami Hernandez <tamianna.h@gmail.com>
2025-05-24 13:28:12 -0500

    refactor: change div to li in ItemCard.jsx. style: add list style type to none in ItemCard.css.

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,10 +2,10 @@ import "./ItemCard.css";
 
 function ItemCard({ item }) {
     return (
-    <div>
+    <li className="card">
         <h2 className="card__title">{item.name}</h2>
         <img className="card__image" src={item.link} alt={item.name}/>
-    </div>
+    </li>
     );
 }
 
