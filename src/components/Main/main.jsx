dc8dbe9f536e2a8aa4ca51eecc7126e9c2299ead
Tami Hernandez <tamianna.h@gmail.com>
2025-05-24 09:46:28 -0500

    feat: create weatherData useState & add to function Main.jsx.

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -2,14 +2,17 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 import { defaultClothingItems } from "../../utils/constants";
 import ItemCard from "../ItemCard/ItemCard";
 
-function Main() {
+function Main({ weatherData }) {
     return (
         <main>
         <WeatherCard />
         <section className="cards">
             <p className="cards__text">Today is 75 &deg; F / You may want to wear:</p>
             <ul className="cards__lists">
-                {defaultClothingItems.map((item) => {
+                {defaultClothingItems
+                .filter((item) => {
+                    return item.weather === weatherData.type;
+                }).map((item) => {
                     return (
                        <ItemCard key={item._id} item={item}/>
                     );
