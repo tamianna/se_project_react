e590b5c053dc9514758e42c55e6454afcd9c4009
Tami Hernandez <tamianna.h@gmail.com>
2025-05-24 09:55:04 -0500

    feat: create, export & import function Footer to App.jsx.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import Footer from "../Footer/Footer";
 
 function App() {
     const [weatherData, setweatherData] = useState({ type: "cold"});
@@ -12,6 +13,7 @@ function App() {
         <div className='page__content'>
             <Header />
             <Main weatherData={weatherData} />
+            <Footer />
         </div>
     </div>
     )
