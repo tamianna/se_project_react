8f037ca3f9dd63d565e8efb089c8d63ec2660481
Tami Hernandez <tamianna.h@gmail.com>
2025-05-24 17:28:41 -0500

    style: add degree to WeatherCard & remove space btw. 75 & F in Main.jsx.

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -8,7 +8,7 @@ function Main({ weatherData, handleCardClick }) {
         <main>
         <WeatherCard />
         <section className="cards">
-            <p className="cards__text">Today is 75 &deg; F / You may want to wear:</p>
+            <p className="cards__text">Today is 75&deg;F / You may want to wear:</p>
             <ul className="cards__lists">
                 {defaultClothingItems
                 .filter((item) => {
--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -4,7 +4,7 @@ import sunny from "../../images/sunny.svg"
 function WeatherCard() {
     return (
         <section className="weather__card">
-            <p className="weather__card-temp">75</p>
+            <p className="weather__card-temp">75&deg;F </p>
             <img src={sunny} alt="Sunny skies." className="weather__card-img" />
         </section>
     )
