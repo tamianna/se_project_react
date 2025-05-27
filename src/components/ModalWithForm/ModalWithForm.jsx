d94391d4b63ddf3c62f341de5ef1769323219b5d
Tami Hernandez <tamianna.h@gmail.com>
2025-05-27 15:34:44 -0500

    refactor: move modal__add-button inside the modal__form. apply prettier format to ModalWithForm.jsx.

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,28 +1,38 @@
-import "./ModalWithForm.css";
-import closeButton from "../../images/closebutton.svg";
+import './ModalWithForm.css'
+import closeButton from '../../images/closebutton.svg'
 
-function ModalWithForm({ children, buttonText, title, isOpen, closeActiveModal }) {
-    return (
-        <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
-            <div className="modal__container">
-                <h2 className="modal__title">{title}</h2>
-                <button 
-                    onClick={closeActiveModal} 
-                    type="button" 
-                    className="modal__close-button"
-                >
-                    <img src={closeButton} alt="Close button, an X." className="modal__close-icon" />
-                </button>
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  isOpen,
+  closeActiveModal,
+}) {
+  return (
+    <div className={`modal ${isOpen ? 'modal_opened' : ''}`}>
+      <div className="modal__container">
+        <h2 className="modal__title">{title}</h2>
+        <button
+          onClick={closeActiveModal}
+          type="button"
+          className="modal__close-button"
+        >
+          <img
+            src={closeButton}
+            alt="Close button, an X."
+            className="modal__close-icon"
+          />
+        </button>
 
-            <form className="modal__form">
-                {children}
-            </form>
-            <button type="submit" className="modal__add-button">
-                {buttonText}
-            </button>
-        </div>
+        <form className="modal__form">
+          {children}
+          <button type="submit" className="modal__add-button">
+            {buttonText}
+          </button>
+        </form>
+      </div>
     </div>
-    )
+  )
 }
 
-export default ModalWithForm;
+export default ModalWithForm
