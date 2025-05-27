87453ee4a8176512da3fca394f4354d8f56facfc
Tami Hernandez <tamianna.h@gmail.com>
2025-05-27 15:15:22 -0500

    style: apply prettier format to Main.jsx.

--- src/components/Main/main.jsx
+++ src/components/Main/main.jsx
@@ -1,42 +1,39 @@
-import "./Main.css";
-import WeatherCard from "../WeatherCard/WeatherCard";
-import { defaultClothingItems } from "../../utils/constants";
-import ItemCard from "../ItemCard/ItemCard";
-import random from "../../images/random.svg";
+import './Main.css'
+import WeatherCard from '../WeatherCard/WeatherCard'
+import { defaultClothingItems } from '../../utils/constants'
+import ItemCard from '../ItemCard/ItemCard'
+import random from '../../images/random.svg'
 
 function Main({ weatherData, handleCardClick }) {
-    return (
-        <main>
-        {weatherData.temp.F !== 999 && (
-  <WeatherCard weatherData={weatherData} />
-        )}
-        <section className="cards">
-            <p className="cards__text">Today is {weatherData.temp.F}&deg;F / You may want to wear:</p>
-            <ul className="cards__lists">
-                {defaultClothingItems
-                .filter((item) => {
-                    return item.weather === weatherData.type;
-                }).map((item) => {
-                    return (
-                       <ItemCard 
-                       key={item._id} 
-                       item={item} 
-                       onCardClick={handleCardClick} 
-                       />
-                    );
-                })}
-            </ul>
-            <button 
-            className="cards__random-button">
-                <img 
-                src={random} 
-                alt="Random icon." 
-                className="random-icon" />
-                Randomize
-            </button>
-        </section>
-        </main>
-    )
+  return (
+    <main>
+      {weatherData.temp.F !== 999 && <WeatherCard weatherData={weatherData} />}
+      <section className="cards">
+        <p className="cards__text">
+          Today is {weatherData.temp.F}&deg;F / You may want to wear:
+        </p>
+        <ul className="cards__lists">
+          {defaultClothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type
+            })
+            .map((item) => {
+              return (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onCardClick={handleCardClick}
+                />
+              )
+            })}
+        </ul>
+        <button className="cards__random-button">
+          <img src={random} alt="Random icon." className="random-icon" />
+          Randomize
+        </button>
+      </section>
+    </main>
+  )
 }
 
-export default Main;
+export default Main
