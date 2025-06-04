080a0ef35affba53ff196f1a8759714876d6143c
Tami Hernandez <tamianna.h@gmail.com>
2025-06-04 11:25:58 -0500

    refactor: change temperature to temp.

--- src/components/ToggleSwitch/ToggleSwith.jsx
+++ src/components/ToggleSwitch/ToggleSwith.jsx
@@ -4,8 +4,8 @@ import "./ToggleSwitch.css";
 const ToggleSwitch = () => {
     return (
         <>
-            <input type="checkbox" id={"tempature-switch-new"} className="toggle__checkbox" />
-            <label htmlFor={"tempature-switch-new"} className="toggle__label">
+            <input type="checkbox" id={"temp-switch-new"} className="toggle__checkbox" />
+            <label htmlFor={"temp-switch-new"} className="toggle__label">
                 <span className={"toggle__button"}></span>
             </label>
         </>
