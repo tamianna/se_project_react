ce0a47fa93b04c7e95b8b70d7ef647e7aeebd33a
Tami Hernandez <tamianna.h@gmail.com>
2025-06-04 11:44:55 -0500

    feat: add celsius to WeatherApi.js.

--- src/utils/WeatherApi.js
+++ src/utils/WeatherApi.js
@@ -29,7 +29,12 @@ export const getWeather = ({ latitude, longitude }, APIkey) => {
 export const filterWeatherData = (data) => {
   const result = {}
   result.city = data.name
-  result.temp = { F: data.main.temp }
+
+  result.temp = {
+    F: data.main.temp,
+    C: Math.round((data.main.temp - 32) * 5 / 9),
+  }
+  
   result.type = getWeatherType(result.temp.F)
   result.condition = data.weather[0].main.toLowerCase()
   result.iconId = data.weather[0].id
