6099a581983498f843baeaecec634cf8a5bf9e79
Tami Hernandez <tamianna.h@gmail.com>
2025-06-06 20:24:18 -0500

    feat: create isFormValid and export. refactor: create on export statement at the bottom of validation.js.

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,11 +1,11 @@
-export const validateName = (name) => {
+const validateName = (name) => {
   if (!name.trim()) return 'Name is required'
   if (name.length < 2) return 'Name must be at least 2 characters'
   if (name.length > 40) return 'Name must be less than 40 characters'
   return ''
 }
 
-export const validateImageUrl = (url) => {
+const validateImageUrl = (url) => {
   if (!url.trim()) return 'Image URL is required'
   try {
     new URL(url)
@@ -15,7 +15,19 @@ export const validateImageUrl = (url) => {
   return ''
 }
 
-export const validateWeather = (weather) => {
+const validateWeather = (weather) => {
   if (!weather) return 'Please select a weather type'
-  return ''
+  return !['hot', 'warm', 'cold'].includes(weather)
+    ? 'Select a weather option'
+    : ''
 }
+
+const isFormValid = (name, imageUrl, weather) => {
+  return (
+    !validateName(name) &&
+    !validateImageUrl(imageUrl) &&
+    !validateWeather(weather)
+  )
+}
+
+export { validateName, validateImageUrl, validateWeather, isFormValid }
