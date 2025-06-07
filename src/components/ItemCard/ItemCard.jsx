72beaacff3948dcad182f46f089846d0cc6a4fd8
Tami Hernandez <tamianna.h@gmail.com>
2025-06-07 12:32:14 -0500

    feat: create function shuffleItems, in ItemCard.jsx.

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,6 +1,15 @@
 import './ItemCard.css'
 import defaultImage from '../../images/defaultimage.jpg'
 
+export function shuffleItems(items = []) {
+  const shuffled = [...items]
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]]
+  }
+  return shuffled
+}
+
 function ItemCard({ item, onCardClick }) {
   const handlePreviewModal = () => {
     onCardClick(item)
