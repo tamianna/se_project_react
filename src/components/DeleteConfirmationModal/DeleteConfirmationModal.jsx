2ba9ad60aa304664cb737575b06441920f52f3c7
Tami Hernandez <tamianna.h@gmail.com>
2025-06-10 15:37:39 -0500

    refactor: create Modal folder, change Modal.js to Modal.jsx and move to Modal folder. import and apply Modal in DeleteConfirmationModal.jsx.

--- src/components/DeleteConfirmationModal/DeleteConfirmationModal.jsx
+++ src/components/DeleteConfirmationModal/DeleteConfirmationModal.jsx
@@ -1,5 +1,5 @@
 import './DeleteConfirmationModal.css'
-import closeButton from '../../images/confirmationclose.svg'
+import { Modal } from '../Modal/Modal'
 
 function DeleteConfirmationModal({
   isOpen,
@@ -8,52 +8,47 @@ function DeleteConfirmationModal({
   isDeleting,
   isCanceling,
 }) {
-   const deleteButtonText = isDeleting ? 'Deleting...' : 'Yes, delete item';
-  const cancelButtonText = isCanceling ? 'Canceling...' : 'Cancel';
+  const deleteButtonText = isDeleting ? 'Deleting...' : 'Yes, delete item'
+  const cancelButtonText = isCanceling ? 'Canceling...' : 'Cancel'
 
-  const isAnyLoading = isDeleting || isCanceling;
+  const isAnyLoading = isDeleting || isCanceling
 
   return (
-    <div className={`modal ${isOpen ? 'modal_opened' : ''}`}>
-      <div className="modal__container modal__confirmation">
-        <div className="modal__confirmation_text">
-          <p className="modal__confirmation-title">
-            Are you sure you want to delete this item?
-          </p>
-          <p className="modal__confirmation-subtitle">
-            This action is irreversible.
-          </p>
-        </div>
-        <button
-          type="button"
-          className="modal__confirmation_close-button"
-          onClick={onClose}
-          disabled={isAnyLoading}
-        >
-          <img src={closeButton} alt="X, for close button." />
-        </button>
-        <form className="modal__delete-form">
-          <div className="modal__confirmation-buttons">
-            <button
-              type="submit"
-              className="modal__confirmation_delete-button"
-              onClick={onConfirm}
-              disabled={isAnyLoading}
-            >
-              {deleteButtonText}
-            </button>
-            <button
-              type="button"
-              className="modal__confirmation_cancel-button"
-              onClick={onClose}
-              disabled={isAnyLoading}
-            >
-              {cancelButtonText}
-            </button>
-          </div>
-        </form>
+    <Modal
+      name="confirmation"
+      onClose={onClose}
+      isOpen={isOpen}
+      disabledClose={isAnyLoading}
+    >
+      <div className="modal__confirmation_text">
+        <p className="modal__confirmation-title">
+          Are you sure you want to delete this item?
+        </p>
+        <p className="modal__confirmation-subtitle">
+          This action is irreversible.
+        </p>
       </div>
-    </div>
+      <form className="modal__delete-form">
+        <div className="modal__confirmation-buttons">
+          <button
+            type="submit"
+            className="modal__confirmation_delete-button"
+            onClick={onConfirm}
+            disabled={isAnyLoading}
+          >
+            {deleteButtonText}
+          </button>
+          <button
+            type="button"
+            className="modal__confirmation_cancel-button"
+            onClick={onClose}
+            disabled={isAnyLoading}
+          >
+            {cancelButtonText}
+          </button>
+        </div>
+      </form>
+    </Modal>
   )
 }
 
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react'
+import closeButton from '../../images/closebutton.svg'
+
+export const Modal = ({ name, isOpen, onClose, children }) => {
+  useEffect(() => {
+    const handleEscape = (e) => {
+      if (e.key === 'Escape') {
+        onClose()
+      }
+    }
+    document.addEventListener('keydown', handleEscape)
+    return () => document.removeEventListener('keydown', handleEscape)
+  }, [onClose])
+
+  const handleOverlay = (e) => {
+    if (e.target === e.currentTarget) {
+      onClose()
+    }
+  }
+
+  if (!isOpen) return null
+
+  return (
+    <div
+      className={`modal modal_type_${name} modal_opened`}
+      onClick={handleOverlay}
+    >
+      <div className="modal__container">
+        {children}
+        <button className="modal__close-button" type="button" onClick={onClose}>
+          <img
+            src={closeButton}
+            alt="Close button, an X."
+            className="modal__close-icon"
+          />
+        </button>
+      </div>
+    </div>
+  )
+}
