dd48a02a7d71cbebd2ffe69b4d14bbd16616c015
Tami Hernandez <tamianna.h@gmail.com>
2025-06-10 11:55:35 -0500

    refactor: remove duplicate markup in function ClothesSection.

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -3,19 +3,7 @@ import React from 'react'
 import ItemCard from '../ItemCard/ItemCard'
 
 function ClothesSection({ handleCardClick, clothingItems }) {
-  if (!clothingItems || clothingItems.length === 0) {
-    return (
-      <div className="clothes-section">
-        <div className="clothes-section_menu">
-          <p className="clothes-section_title">Your items</p>
-          <button type="button" className="clothes-section_button">
-            + Add new
-          </button>
-        </div>
-        <p className="clothes-section__empty">No clothing items found.</p>
-      </div>
-    )
-  }
+  const noItems = !clothingItems || clothingItems.length === 0
 
   return (
     <div className="clothes-section">
@@ -25,17 +13,21 @@ function ClothesSection({ handleCardClick, clothingItems }) {
           + Add new
         </button>
       </div>
-      <ul className="cards__lists">
-        {clothingItems.map((item) => {
-          return (
-            <ItemCard
-              key={item._id}
-              item={item}
-              onCardClick={handleCardClick}
-            />
-          )
-        })}
-      </ul>
+      {noItems ? (
+        <p className="clothes-section__empty">No clothing items found.</p>
+      ) : (
+        <ul className="cards__lists">
+          {clothingItems.map((item) => {
+            return (
+              <ItemCard
+                key={item._id}
+                item={item}
+                onCardClick={handleCardClick}
+              />
+            )
+          })}
+        </ul>
+      )}
     </div>
   )
 }
