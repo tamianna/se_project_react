2ca12a62ad47cd7625df54e4b9421b97df7584fa
Tami Hernandez <tamianna.h@gmail.com>
2025-07-23 10:05:29 -0500

    feat: import useContext & CurrentUserContext. create const currentuser, avatar, name & renderAvatar in src/components/SideBar/SideBar.jsx.

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,8 +1,23 @@
 import './SideBar.css'
-import React from 'react'
+import React, { useContext } from 'react'
 import headerAvatar from '../../images/header-avatar.svg'
+import CurrentUserContext from '../../contexts/CurrentUserContext'
 
-function SideBar() {
+function SideBar({ onEditProfle }) {
+  const currentUser = useContext(CurrentUserContext)
+
+  const avatar = currentUser?.avatar
+  const name = currentUser?.name || 'Anonymous'
+
+  const renderAvatar = () =>
+    avatar ? (
+      <img className="sidebar__avatar" src={avatar} alt="User avatar." />
+    ) : (
+      <div className="sidebar__avatar sidebar__avatar_placeholder">
+        {name[0].toUpperCase()}
+      </div>
+    )
+
   return (
     <div className="sidebar">
       <img
